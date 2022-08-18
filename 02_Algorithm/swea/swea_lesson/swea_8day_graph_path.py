05a79e3f66d28546a18578d4bfe7be04945e5169
Kwon <power916@daum.net>
2022-08-19 00:44:09 +0900

    220819 TIL

--- 02_Algorithm/swea/swea_lesson/swea_8day_graph_path.py
+++ 02_Algorithm/swea/swea_lesson/swea_8day_graph_path.py
@@ -11,17 +11,25 @@ for tc in range(1, T + 1):
     STACK = [S]
     visited[S] = 1
 
-    while True:
+    while len(STACK) != 0:
 
         for x in line:
             if x[0] == STACK[-1] and visited[x[1]] == 0:
-                STACK += [x[1]]
-                visited[x[1]] = 1
+                STACK.pop()
+                num = x[1]
+                visited[num] = 1
                 break
 
-        if STACK[-1] == G:
+        for x in line:
+            if STACK[-1] == x[1] and visited[x[1]] == 1 and visited[x[0]] == 1:
+                STACK.pop()
+                num = x[0]
+                break
+
+        if num == G:
             result = 1
             break
-        elif STACK[-1] != G:
+        else:
+            STACK += [num]
 
     print(f'#{tc} {result}')
