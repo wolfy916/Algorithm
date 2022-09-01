440b1f20a2e4d1fa19bc78b658f048ab9ea8db28
Kwon <power916@daum.net>
2022-09-02 00:36:16 +0900

    220902 TIL

--- 02_Algorithm/baekj/bk_2110.py
+++ 02_Algorithm/baekj/bk_2110.py
@@ -1,38 +1,32 @@
-def BinarySearch(s, e, m, c, d):  # s : 시작점 인덱스, e : 끝점 인덱스, m : 중간값 인덱스, c : 남은 횟수
-    if c == 0:
-        return d
-
-    left_distance = houses[m] - houses[s]
-    right_distance = houses[e] - houses[m]
-
-    if left_distance < right_distance:
-        return BinarySearch(m, e, (m + e) // 2, c - 1, left_distance)
-    elif left_distance > right_distance:
-        return BinarySearch(s, m, (s + m) // 2, c - 1, right_distance)
-    else:
-        a = BinarySearch(m, e, (m + e) // 2, c - 1, left_distance)
-        b = BinarySearch(s, m, (s + m) // 2, c - 1, right_distance)
-        if a >= b:
-            return b
-        else:
-            return a
-
+# 공유기 설치
 
 N, C = map(int, input().split())
 houses = [int(input()) for house in range(N)]
-houses.sort()
 
-if C > 2:
-    if N % 2:
-        maxV = BinarySearch(0, N - 1, (N - 1) // 2, C - 2, N)
-    else:
-        maxV1 = BinarySearch(0, N - 1, (N - 1) // 2, C - 2, N)
-        maxV2 = BinarySearch(0, N - 1, ((N - 1) // 2) + 1, C - 2, N)
-        if maxV1 >= maxV2:
-            maxV = maxV1
-        else:
-            maxV = maxV2
-else:
-    maxV = houses[-1] - houses[0]
+houses.sort()  # 집들을 오름차순으로 정렬
 
-print(maxV)
+start = 1
+end = houses[-1] - houses[0]
+d_ls = [houses[0]]  # d_ls : device_locations ; 공유기를 설치할 집 리스트
+result = 0
+while start < end:
+
+    s_d = (start + end) // 2  # s_d : standard_device ; 기준이 되는 거리
+    d_cnt = 1                 # d_cnt : device_count ; 해당 s_d로 몇 번 설치가 가능한지 기록
+    for house in houses:             #
+        if s_d <= house - d_ls[-1]:  # 마지막으로 설치된 공유기와 집간의 거리가 기준 거리 이상이면 공유기 설치
+            d_ls += [house]          # 공유기 설치 리스트에 append
+            d_cnt += 1               # 공유기 설치 개수 += 1
+
+    if d_cnt >= C:
+        result = s_d
+        start = s_d + 1
+    elif d_cnt < C:
+        end = s_d
+
+    d_ls = [houses[0]]  # d_ls 초기화
+
+if C == 2:
+    result = houses[-1] - houses[0]
+
+print(result)
