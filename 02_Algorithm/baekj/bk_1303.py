ce354de43f6c2f85a963db320e44e58c62f20a8e
Kwon <power916@daum.net>
2022-09-18 17:01:08 +0900

    220918 TIL

--- 02_Algorithm/baekj/bk_1303.py
+++ 02_Algorithm/baekj/bk_1303.py
@@ -1,25 +1,33 @@
 # 전쟁-전투
 
-def dfs(s_i, s_j, color):
-    STACK = [[s_i, s_j]]
-    while STACK:
-        STACK
+def stack_method(i, j, color):
+    cnt = 1            # 병사수 카운트
+    stack = [[i, j]]   # stack 시작 인덱스를 초기값으로 설정
+    visited[i][j] = 1  # visited
+    while stack:
+        i, j = stack.pop()  # pop
+        for di, dj in delta:
+            ni, nj = i+di, j+dj
+            if 0 <= ni < M and 0 <= nj < N:  # 유효 인덱스 검사
+                if area[ni][nj] == color and visited[ni][nj] != 1:  # 색깔, visited 검사
+                    stack += [[ni, nj]]  # push
+                    visited[ni][nj] = 1  # visited
+                    cnt += 1
+    return cnt**2  # 제곱값 리턴
+
 
 N, M = map(int, input().split())
 area = [list(input()) for _ in '_'*M]
 
-delta = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-visited = [[0]*N for _ in '_'*M]
-result_W = 0
-result_B = 0
-
+delta = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 상하좌우 탐색 도구
+visited = [[0]*N for _ in '_'*M]  # visited list
+W = B = 0  # 최종 출력값
 for i in range(M):
     for j in range(N):
-        if visited[i][j] == 0:
-            if area[i][j] == 'W':
-                visited[i][j] = 1
-                dfs(i, j, 1)
+        if visited[i][j] == 0:     # 방문한 곳이 아니라면
+            if area[i][j] == 'W':  # 색깔에 따라 함수 호출
+                W += stack_method(i, j, 'W')
             else:
-                visited[i][j] = 2
-                dfs(i, j, 2)
+                B += stack_method(i, j, 'B')
 
+print(W, B)
--- 02_Algorithm/baekj/bk_2210.py
+++ 02_Algorithm/baekj/bk_2210.py
@@ -1,27 +1,27 @@
 # 숫자판 점프
-from itertools import product
+from itertools import product  # product : 숫자 중복 허용 순열
 
 area = [list(map(int, input().split())) for _ in '_'*5]
 
 cnt = 0
-delta = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-num_list = []
+delta = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 5번을 이동하기 위한 delta
+num_list = []  # 완성된 숫자들을 할당할 리스트
 for i in range(5):
     for j in range(5):
-        for p in list(product([0, 1, 2, 3], repeat=5)):
-            num = [area[i][j]]
+        for p in list(product([0, 1, 2, 3], repeat=5)):  # p : 많은 순열 중 하나
+            num = [area[i][j]]  # 숫자를 완성하기 위한
             ni, nj = i, j
-            for k in p:
+            for k in p:  # p : delta를 사용하기 위한 인덱스 순열
                 ni += delta[k][0]
                 nj += delta[k][1]
-                if 0 <= ni < 5 and 0 <= nj < 5:
-                    num += [area[ni][nj]]
-                else:
-                    break
-            else:
-                if num in num_list:
+                if 0 <= ni < 5 and 0 <= nj < 5:  # 유효 인덱스 검사
+                    num += [area[ni][nj]]        # 숫자 추가
+                else:      # 유효한 인덱스가 아니라면
+                    break  # 6개의 숫자를 완성하는 도중에 break
+            else:  # break 없이 정상적으로 for문을 종료하여 숫자 완성했을 경우
+                if num in num_list:  # 완성한 숫자가 이미 있다면 continue
                     continue
                 else:
-                    num_list += [num]
-                    cnt += 1
+                    num_list += [num]  # 추가
+                    cnt += 1  # 완성된 숫자 갯수 + 1
 print(cnt)
