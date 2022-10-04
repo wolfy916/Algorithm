eec5b84e31ea7715ad9ac84209497be6735e9133
Kwon <power916@daum.net>
2022-10-04 22:44:17 +0900

    221004 TIL

--- 02_Algorithm/swea/swea_lesson/algorithm_skill/mst/kruskal.py
+++ 02_Algorithm/swea/swea_lesson/algorithm_skill/mst/kruskal.py
@@ -1,10 +1,10 @@
 def find_set(x):
-    while x!=rep[x]:
-        x = rep[x]
+    while x!=par[x]:
+        x = par[x]
     return x
 
 def union(x, y):
-    rep[find_set(y)] = find_set(x)
+    par[find_set(y)] = find_set(x)
 
 V, E = map(int, input().split())    # V 마지막 정점, 0~V번 정점. 개수 (V+1)개
 edge = []
@@ -12,7 +12,7 @@ for _ in range(E):
     u, v, w = map(int, input().split())
     edge.append([u, v, w])
 edge.sort(key=lambda x:x[2])
-rep = [i for i in range(V+1)]       # 대표원소 배열
+par = [i for i in range(V+1)]       # 대표원소 배열
 
 N = V + 1   # 실제 정점 수
 cnt = 0     # 선택한 edge의 수
