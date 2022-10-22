574dfc78fd9593320ac30ee780a34df7a82c45cd
Kwon <power916@daum.net>
2022-10-22 20:09:35 +0900

    파일정리

--- 03_Web/221018_Tues/clubs/urls.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urlparse
-from django.urls import path
-from . import views
-
-app_name = 'clubs'
-urlpatterns = [
-    path('clubs/', views.club_list, name='club_list'),
-]
