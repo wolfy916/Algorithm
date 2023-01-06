1a7396cecc3ed8b61f8a2e74f57e05f23b8c2490
권태형 <power916@daum.net>
2023-01-06 11:32:15 +0900

    230106 TIL

--- 02_Algorithm/baekj/bk_6087.py
+++ 02_Algorithm/baekj/bk_6087.py
@@ -7,37 +7,34 @@ minV = 10000
 for i in range(H):
     for j in range(W):
         if area[i][j] == 'C':
-            visited = [[0] * W for _ in '_' * H]
-            visited[i][j] = 1
-            direction = [[0 for w in range(W)] for _ in '_' * H]
-            direction[i][j] = -1
-            q = [(i, j, -1)]
+            visited = [[minV] * W for _ in '_' * H]
+            visited[i][j] = 0
+            q = []
+            for k in range(4):
+                di, dj = delta[k]
+                ni, nj = i + di, j + dj
+                if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*':
+                    visited[ni][nj] = 0
+                    q.append((ni, nj, k))
             while q:
                 vi, vj, d = q.pop(0)
                 if visited[vi][vj] > minV:
                     continue
                 if area[vi][vj] == 'C' and (vi, vj) != (i, j):
-                    minV = min(minV, visited[vi][vj] - 1)
+                    minV = min(minV, visited[vi][vj])
                 else:
                     for k in range(4):
                         di, dj = delta[k]
                         ni, nj = vi + di, vj + dj
-                        if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*':
-                            if not visited[ni][nj] or visited[ni][nj] > visited[vi][vj]:
-                                if d == -1:
-                                    visited[ni][nj] = visited[vi][vj]
-                                else:
-                                    if d == k:
-                                        visited[ni][nj] = visited[vi][vj]
-                                    else:
-                                        visited[ni][nj] = visited[vi][vj] + 1
-                                direction[ni][nj] = k
-                                q.append((ni, nj, k))
-                            elif visited[ni][nj] == visited[vi][vj] and direction[ni][nj] != k:
-                                direction[ni][nj] = k
-                                q.append((ni, nj, k))
+                        # 유효성 검사 & 벽이 아님
+                        if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*' and visited[ni][nj] >= visited[vi][vj]:
+                            if d == k:
+                                visited[ni][nj] = min(visited[ni][nj], visited[vi][vj])
+                            else:
+                                visited[ni][nj] = min(visited[ni][nj], visited[vi][vj]+1)
+                            
             print(minV)
-            for _ in range(H):
-                print(*visited[_])
+            # for _ in range(H):
+            #     print(*visited[_])
             exit()
             # direction[ni][nj] != k
--- 02_Algorithm/baekj/practice2.py
+++ 02_Algorithm/baekj/practice2.py
@@ -1,37 +1,34 @@
-# 스도쿠
-
-sudoku = []
-for i in range(9):
-    line = list(map(int, input().split()))
-
-flag = 1
-while flag:
-    flag = 0
-    for i in range(9):
-        for j in range(9):
-            if sudoku[i][j] == 0:
-                flag = 1
-                if sudoku[i].count(0) == 1:
-                    sudoku[i][j] = 45 - sum(sudoku[i])
+W, H = map(int, input().split())
+area = [list(input()) for _ in '_'*H]
+delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+minV = 10000
+for i in range(H):
+    for j in range(W):
+        if area[i][j] == 'C':
+            visited = [[minV] * W for _ in '_' * H]
+            visited[i][j] = 1
+            q = [(i, j, -1)]
+            while q:
+                vi, vj, d = q.pop(0)
+                if visited[vi][vj] > minV:
+                    continue
+                if area[vi][vj] == 'C' and (vi, vj) != (i, j):
+                    minV = min(minV, visited[vi][vj] - 1)
                 else:
-                    zero = 0
-                    sumV = 0
-                    for ii in range(9):
-                        if sudoku[ii][j] == 0:
-                            zero += 1
-                        sumV += sudoku[ii][j]
-                    if zero == 1:
-                        sudoku[i][j] = 45 - sumV
-                    else:
-                        row, col = i//3*3, j//3*3
-                        zero = 0
-                        sumV = 0
-                        for x in range(row, row+3):
-                            for y in range(col, col+3):
-                                if sudoku[x][y] == 0:
-                                    zero += 1
-                                sumV += sudoku[x][y]
-                        if zero == 1:
-                            sudoku[i][j] = 45 - sumV
-for _ in range(9):
-    print(*sudoku[_])
+                    for k in range(4):
+                        di, dj = delta[k]
+                        ni, nj = vi + di, vj + dj
+                        if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*':
+                            if not visited[ni][nj] or visited[ni][nj] > visited[vi][vj]:
+                                if d == -1:
+                                    visited[ni][nj] = visited[vi][vj]
+                                else:
+                                    if d == k:
+                                        visited[ni][nj] = visited[vi][vj]
+                                    else:
+                                        visited[ni][nj] = visited[vi][vj] + 1
+                                q.append((ni, nj, k))
+            print(minV)
+            for _ in range(H):
+                print(*visited[_])
+            exit()
