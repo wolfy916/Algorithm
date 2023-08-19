2127e24fec4e9d332ce2d36c0fcd1e5665b4c587
Kwon <power916@daum.net>
2023-08-20 00:12:11 +0900

    230820 algorithm

--- Algorithm_Solution/problems/pracitce.py
+++ Algorithm_Solution/problems/pracitce.py
@@ -1,26 +1,50 @@
-# 풀이 1. 완전 탐색
-import sys
+def solution(gems):
+    # [1] 각 보석에 대한 사용 여부 리스트 생성
+    gem_type = set(gems)  # 보석 종류
+    gem_lst = list(gem_type)
+    del gem_type
+    N, M = len(gems), len(gem_lst)  # 전체 길이, 보석 종류 개수
+    ref = {gem_lst[i]: i for i in range(M)}
+    used = [0] * M
 
-# [A] 입력 함수 초기화
-def input():
-    return sys.stdin.readline().rstrip('\n')
+    # [2] 투 포인터
+    p1 = p2 = 0
+    min_length = N
+    answer = [1, N]
+    cnt = 0
+    while p1 < N or p2 < N:
+        # [2-1] 탐색 구간이 모든 보석 종류를 모을 때까지 p2 이동
+        while p2 < N and cnt < M:
+            used[ref[gems[p2]]] += 1
+            if used[ref[gems[p2]]] == 1:
+                cnt += 1
+            p2 += 1
 
-TC = int(input())
-for _ in range(TC):
-    M, N, x, y = map(int, input().split())
-    ans = 0
-    a = b = 0
-    print(a, b, ans)
-    while a != x or b != y:
-        a = (a + 1) % (M + 1)  # 1씩 더하는 귀요미
-        if a == 0: a = 1
-        b = (b + 1) % (N + 1)  #
-        if b == 0: b = 1
-        ans += 1
-        if b == y:
-            print(a, b, ans)
-        if (a, b) == (M, N):
-            if (x, y) != (M, N):
-                ans = -1
-            break
-    print(ans)
+        # [2-2] 모든 보석 종류를 모았으며, 기준값 보다 구간의 길이가 작다면 갱신
+        if cnt >= M and min_length > p2 - p1:
+            min_length = p2 - p1
+            answer = [p1 + 1, p2]
+
+        # [2-3] 좌측 포인터인 p1을 1만큼 이동
+        used[ref[gems[p1]]] -= 1
+        if used[ref[gems[p1]]] == 0:
+            cnt -= 1
+        p1 += 1
+
+    return answer
+
+Gems = [
+    ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"],
+    ["AA", "AB", "AC", "AA", "AC"],
+    ["XYZ", "XYZ", "XYZ"],
+    ["ZZZ", "YYY", "NNNN", "YYY", "BBB"],
+    ["A", "B", "B", "C", "B", "B", "B", "B", "C", "A"],
+]
+
+for i in range(5):
+    print(f"#{i+1} {solution(Gems[i])}")
+
+#1 [3, 7]
+#2 [1, 3]
+#3 [1, 1]
+#4 [1, 5]
--- Algorithm_Solution/problems/practice2.py
+++ Algorithm_Solution/problems/practice2.py
@@ -1,18 +1,99 @@
-import time
+from collections import deque as dq
 
-def check(n):
-    cnt = 0
-    for m in range(2, int(n ** 0.5) + 1):
-        if n % m == 0:
-            if cnt > 0: return False
-            cnt += 1
-    return True if cnt == 1 else False
+# [A] 카드의 좌표를 원소로 갖는 리스트를 리턴하는 함수
+def search_card(board, N, M):
+    cards = []
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 0: continue
+            cards.append((i, j))
+    return cards
 
-start = time.time()
-N, M = 10, 10000
-answer = 0
-for num in range(N, M):
-    if check(num):
-        answer += 1
-print(answer)
-print(time.time() - start)
+# [B] S 지점에서 E 지점으로 이동하는 최소비용을 리턴하는 함수
+def bfs(board, N, M, si, sj, ei, ej, log):
+    delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    q = dq([(si, sj)])
+    visited = [[100] * M for _ in range(N)]
+    visited[si][sj] = 0
+    found_cards = []
+    found_cards_cnt = []
+    while q:
+        vi, vj = q.popleft()
+        if len(log) % 2:
+            if (vi, vj) == (ei, ej): return visited[vi][vj] + 1
+        else:
+            if board[vi][vj] and (vi, vj) not in log:
+                for f_card in found_cards:
+                    i, j = f_card
+                    if board[vi][vj] == board[i][j]:
+                        break
+                else:
+                    found_cards.append((vi, vj))
+                    found_cards_cnt.append(visited[vi][vj] + 1)
+        for di, dj in delta:
+            ni, nj = vi + di, vj + dj
+            if ni < 0 or nj < 0 or ni >= N or nj >= M: continue
+            if visited[vi][vj] + 1 < visited[ni][nj]:
+                visited[ni][nj] = visited[vi][vj] + 1
+                q.append((ni, nj))
+            ti, tj = ni, nj
+            while True:
+                if ni < 0 or nj < 0 or ni >= N or nj >= M:
+                    ni -= di; nj -= dj;
+                    break
+                if board[ni][nj] and (ni, nj) not in log: break
+                ni += di; nj += dj;
+            if (ti, tj) == (ni, nj): continue
+            if visited[vi][vj] + 1 < visited[ni][nj]:
+                visited[ni][nj] = visited[vi][vj] + 1
+                q.append((ni, nj))
+    return found_cards, found_cards_cnt
+
+# [C] dfs 탐색 함수
+def dfs(board, vi, vj, N, M, cards, log, cnt):
+    global answer
+    if cnt > answer: return
+    if len(cards) == len(log):
+        answer = min(answer, cnt)
+        return
+    if len(log) % 2:
+        for ei, ej in cards:
+            if (vi, vj) == (ei, ej): continue
+            if board[vi][vj] == board[ei][ej]:
+                break
+        add = bfs(board, N, M, vi, vj, ei, ej, log)
+        dfs(
+            board, ei, ej, N, M,
+            cards, log + [(ei, ej)],
+            cnt + add
+        )
+    else:
+        f_cards, f_cards_cnt = bfs(board, N, M, vi, vj, 0, 0, log)
+        for card, add in zip(f_cards, f_cards_cnt):
+            si, sj = card
+            dfs(
+                board, si, sj, N, M,
+                cards, log + [(si, sj)],
+                cnt + add
+            )
+
+# [D] 메인 함수
+def solution(board, r, c):
+    global answer
+    answer = 100
+    N, M = len(board), len(board[0])
+    cards = search_card(board, N, M)
+    dfs(board, r, c, N, M, cards, [], 0)
+    return answer
+
+B = [
+    [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]],
+    [[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]],
+    [[1,2,3,5], [4,1,2,6], [3,4,5,0], [6,0,0,0]],
+    [[1, 5, 0, 2], [6, 4, 3, 0], [0, 2, 1, 5], [3, 0, 6, 4]],
+    [[1,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,1]]
+]
+R = [1, 0, 0, 0, 0]
+C = [0, 1, 0, 0, 0]
+for i in range(5):
+    print(solution(B[i], R[i], C[i]))
