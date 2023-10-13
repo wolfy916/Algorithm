8598128240d2abd90bafdaa518ccfee748497dac
Kwon <power916@daum.net>
2023-10-13 18:29:11 +0900

    231013 algorithm

--- Algorithm_Solution/baekj/bk_6087.py
+++ Algorithm_Solution/baekj/bk_6087.py
@@ -1,40 +1,55 @@
 # 레이저 통신
+import sys
+from heapq import heappop, heappush
 
-W, H = map(int, input().split())
-area = [list(input()) for _ in '_'*H]
-delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-minV = 10000
-for i in range(H):
-    for j in range(W):
-        if area[i][j] == 'C':
-            visited = [[minV] * W for _ in '_' * H]
-            visited[i][j] = 0
-            q = []
-            for k in range(4):
-                di, dj = delta[k]
-                ni, nj = i + di, j + dj
-                if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*':
-                    visited[ni][nj] = 0
-                    q.append((ni, nj, k))
-            while q:
-                vi, vj, d = q.pop(0)
-                if visited[vi][vj] > minV:
-                    continue
-                if area[vi][vj] == 'C' and (vi, vj) != (i, j):
-                    minV = min(minV, visited[vi][vj])
+# [A] 입력 함수 초기화
+def input():
+    return sys.stdin.readline().rstrip('\n')
+
+# [B] bfs + heapq
+def bfs(si, sj):
+    delta = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+    visited = [[[float('inf')] * 4 for _ in range(W)] for _ in range(H)]
+    for k in range(4):
+        visited[si][sj][k] = 0
+    q = [(-1, si, sj)]
+    while q:
+        d, vi, vj = q.pop(0)
+        if area[vi][vj] == 'C' and (si, sj) != (vi, vj):
+            ti, tj = vi, vj
+            continue
+        for k in range(4):
+            if d != -1 and (d + 2) % 4 == k: continue
+            ni, nj = vi + delta[k][0], vj + delta[k][1]
+            if ni < 0 or nj < 0 or ni >= H or nj >= W: continue
+            if area[ni][nj] == '*': continue
+            if d != -1:
+                if (d + 1) % 4 == k or (k + 1) % 4 == d:
+                    if visited[vi][vj][d] + 1 >= visited[ni][nj][k]: continue
+                    visited[ni][nj][k] = visited[vi][vj][d] + 1
                 else:
-                    for k in range(4):
-                        di, dj = delta[k]
-                        ni, nj = vi + di, vj + dj
-                        # 유효성 검사 & 벽이 아님
-                        if 0 <= ni < H and 0 <= nj < W and area[ni][nj] != '*' and visited[ni][nj] >= visited[vi][vj]:
-                            if d == k:
-                                visited[ni][nj] = min(visited[ni][nj], visited[vi][vj])
-                            else:
-                                visited[ni][nj] = min(visited[ni][nj], visited[vi][vj]+1)
-                            
-            print(minV)
-            # for _ in range(H):
-            #     print(*visited[_])
-            exit()
-            # direction[ni][nj] != k
+                    if visited[vi][vj][d] >= visited[ni][nj][k]: continue
+                    visited[ni][nj][k] = visited[vi][vj][d]
+            else:
+                visited[ni][nj][k] = visited[vi][vj][d]
+            q.append((k, ni, nj))
+
+    return min(visited[ti][tj])
+
+# [main]
+if __name__ == '__main__':
+    # 데이터 입력 및 초기화
+    W, H = map(int, input().split())
+    area = [list(input()) for _ in range(H)]
+
+    # 시작 지점 탐색
+    si, sj = -1, -1
+    for i in range(H):
+        for j in range(W):
+            if area[i][j] != 'C': continue
+            si, sj = i, j
+            break
+        if si != -1: break
+
+    # bfs 결과 출력
+    print(bfs(si, sj))
